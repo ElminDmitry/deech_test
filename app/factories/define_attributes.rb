c5eef6aa8fb85c1ed9da2492bc6940baa826ecc3
Ivan Fadeev <ivanfadeev91@gmail.com>
2017-12-14 10:56:14 +0200

    DefineAttributes module refactoring

--- app/factories/define_attributes.rb
+++ app/factories/define_attributes.rb
@@ -6,33 +6,35 @@ module DefineAttributes
 
   # ClassMethods
   module ClassMethods
-	def define_attributes(attrs_list)
-	  attr_accessor(*attrs_list, :page)
+    # rubocop:disable Metrics/MethodLength
+    # rubocop:disable Metrics/AbcSize
+    def define_attributes(attrs_list)
+      attr_accessor(*attrs_list, :page)
 
-	  define_method('initialize') do |page_id|
-	  	@page = if page_id
-	  	          Page.find(page_id)
-	  	        else
-			      Page.new(page_type: 'page')
-		        end
+      define_method('initialize') do |page_id = nil|
+        @page = if page_id
+                  Page.find(page_id)
+                else
+                  Page.new(page_type: 'page')
+                end
 
-	    attrs_list.each do |attribute|
-	      eval("@#{attribute} = page_#{attribute}")
-	    end
-	  end
+        attrs_list.each do |attribute|
+          instance_variable_set("@#{attribute}", send("page_#{attribute}"))
+        end
+      end
 
-	  attrs_list.each do |attribute|
-	    define_method("page_#{attribute}") do
-	      attribute_obj = Attribute.find_by(name: attribute.to_s)
-	      if page.persisted?
-	      	page.attribute_values.find_by(page_attribute: attribute_obj)
-	      else
-	      	page.attribute_values.new(page_attribute: attribute_obj)
-	      end
-	    end
+      attrs_list.each do |attribute|
+        define_method("page_#{attribute}") do
+          attribute_obj = Attribute.find_by(name: attribute.to_s)
+          if page.persisted?
+            page.attribute_values.find_by(page_attribute: attribute_obj)
+          else
+            page.attribute_values.new(page_attribute: attribute_obj)
+          end
+        end
 
-	    private "page_#{attribute}".to_sym
-	  end
-	end
+        private "page_#{attribute}".to_sym
+      end
+    end
   end
 end
